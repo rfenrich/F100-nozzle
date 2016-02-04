6c861cf0d6d686594d30a3357ca2e3a377e4d0d4
Rick Fenrich <rfenrich@stanford.edu>
2016-02-04 13:09:00 -0800

    added example optimization driver and wrapper

---
+++ exampleOptDriver.m
@@ -0,0 +1,86 @@
+% exampleOptDriver.m runs a deterministic optimization of nozzle shape 
+% for maximum thrust using Matlab's fmincon. The nozzle shape is
+% is parameterized using a NURB spline (B-spline). There are 9 design
+% variables, which correspond to the x and y coordinates of 5 control
+% points. Length and inlet size of the nozzle is fixed. The optimization 
+% formulation is:
+%
+% min                    weight
+% s.t.             thrust >= 25,000 N
+%       44 kg/s <= mass flow rate <= 46 kg/s  --> for a well posed problem
+%                       Ax <= b               --> constraints on relative control point movement
+%                    lb <= x <= b             --> constraints on individual control point movement
+%       where x is the vector of design variables
+%
+% Rick Fenrich 2/4/16
+
+clear all; close all; clc;
+
+% Set up initial B-spline shape
+Dinlet = 0.3255*2;
+knots = [0 0 0 1 2 3 4 5 5 5]';
+coefs(1,:) = [0 2.3121 2.4845 2.5921 2.9799 3.8214 4.6055];
+coefs(1,:) = coefs(1,:) - 0.33*coefs(1,end);
+coefs(1,1) = 0;
+coefs(1,:) = coefs(1,:)/4.6055;
+coefs(2,:) = [0.3255 0.3255 0.3253 0.2285 0.1673 0.2615 0.2633];
+coefs(2,:) = coefs(2,:)*0.33;
+coefs(2,:) = coefs(2,:) + 0.3255 - coefs(2,1);  
+
+% Set up design variables (x and y coordinates of middle 2 control points, 
+% and y coordinate of last control point)
+x(1:4) = coefs(1,3:6);
+x(5:9) = coefs(2,3:7);
+
+% Set up ranges of design variables
+lb = 0.8*x;
+ub = 1.2*x;
+
+n = length(x); % number of design variables
+
+% Set up linear inequality constraints
+A = zeros(11,9); % 11 constraints total
+b = zeros(11,1); 
+delta = 1e-3; % delta for spacing 
+
+% x position constraints
+A(1,1) = -1; b(1) = -coefs(1,2)*10; % 3rd control point should remain to right of 2nd
+A(2,1) = 1; A(2,2) = -1; b(2) = -delta*10; % 4th control point should remain to right of 3rd c.p.
+A(3,2) = 1; A(3,3) = -1; b(3) = -delta*10; % 5th c.p.  " "
+A(4,3) = 1; A(4,4) = -1; b(4) = -delta*10; % 6th c.p.  " "
+A(5,4) = 1; b(5) = coefs(1,end)-delta*10; % 6th c.p. should remain to left of 7th c.p.
+A(6,7) = 1; A(6,6) = -1; b(6) = -delta; % 5th c.p. should remain below 4th c.p.
+A(7,6) = 1; b(7) = Dinlet/2; % 4th c.p. should be below inlet height
+A(8,7) = 1; A(8,5) = -1; b(8) = -delta; % 5th c.p. should remain below 3rd c.p.
+A(9,5) = 1; b(9) = Dinlet/2; % 3rd c.p. should be below inlet height
+A(10,7) = 1; A(10,8) = -1; b(10) = -delta; % 6th c.p. should remain above 5th c.p.
+A(11,7) = 1; A(11,9) = -1; b(11) = -delta*10; % 7th c.p. should remain above 5th c.p.
+
+% Set linear equality constraints so that nozzle inlet is fixed size
+beq = [];
+Aeq = [];
+
+% Set nonlinear inequality constraint function
+nonlconFun = @(r) exampleWrapper2(r,knots,coefs,'nonlcon');
+
+% Set optimization options
+options.MaxIter = 50;
+options.Algorithm = 'sqp';
+options.Display = 'iter-detailed';
+options.FinDiffRelStep = 1e-3;
+opttions.TolX = 1e-10;
+%options.FunValCheck = 'on';
+
+% Print data to screen
+fprintf('Number design variables: %i\n',n);
+
+objFun = @(r) exampleWrapper(r,knots,coefs,'volume');
+
+figure;
+hold on;
+tic;
+[sol,val] = fmincon(objFun,x,A,b,Aeq,beq,lb,ub,nonlconFun,options);
+timeToEnd = toc;
+fprintf('Time to completion: %0.2 sec\n',timeToEnd);
+
+
---
+++ exampleWrapper.m
@@ -0,0 +1,139 @@
+function [varargout] = exampleWrapper(x,knots,coefs,output)
+% Example wrapper for an optimization using nonIdealNozzle.m. This wrapper
+% in particular corresponds to exampleOptDriver.m. The inputs knots, coefs,
+% and objective can be defined inside this wrapper, so that exampleWrapper
+% is only a function of x, the design variables.
+%
+% exampleOptDriver.m uses the x and y coordinates of the control points of
+% a B-spline parameterized nozzle geometry as design variables to optimize
+% deterministically for max thrust.
+%
+% INPUTS:
+% x = input vector of design variables
+% knots = knots vector required for B-spline parameterization
+% coefs = coefficient matrix required for B-spline parameterization
+% output = string containing name of output variable
+%
+% OUTPUTS:
+% varargout = output of wrapper specified by objective string in inputs
+%
+% Rick Fenrich 2/1/16
+
+% ====================== ASSIGN DESIGN VARIABLES =========================
+
+% Redefine B-spline geometry using design variables
+coefs(1,3:6) = x(1:4);
+coefs(2,3:7) = x(5:9);
+nozzle.geometry.bSpline.knots = knots;
+nozzle.geometry.bSpline.coefs = coefs;
+fprintf('%0.4f ',coefs(1,:));
+fprintf('\n');
+fprintf('%0.4f ',coefs(2,:));
+fprintf('\n');
+
+% ====================== SET CONSTRAINT CONSTANTS ========================
+minRequiredThrust = 25000; % N
+minMassFlowRate = 44; % kg/s
+maxMassFlowRate = 46; % kg/s
+
+% ========================== PREPARE INPUTS ==============================
+
+% ------------------------ SET FLIGHT REGIME -----------------------------
+% High speed, high altitude case
+altitude = 35000; % in feet
+mach = 0.9;
+nozzle.inlet.Tstag = 1021.5;
+nozzle.inlet.Pstag = 1.44925e5;
+
+% --------------------- SET HEAT TRANSFER PARAMS -------------------------
+nozzle.hInf = 500; % W/m^2-K, heat transfer coefficient from external nozzle wall to environment
+
+% --------------------- SET MATERIAL PROPERTIES --------------------------
+% SEPCARBINOX A500, a ceramic matrix composite
+nozzle.wall.k = 8.6; % W/m*K, thermal conductivity of nozzle wall
+nozzle.wall.coeffThermalExpansion = 2.3e-6; % 1/K, coefficient of thermal expansion of nozzle wall
+nozzle.wall.E = 80e9; % Pa, elastic modulus of nozzle wall
+nozzle.wall.poissonRatio = 0.3; % Poisson ratio of nozzle wall
+
+% ------------------- SET ERROR TOLERANCE RANGES -------------------------
+error.betweenIterations.inletMach = 1e-10;
+error.solver.inletMach = 1e-8;
+error.betweenIterations.exitTemp = 1e-6;
+error.solver.apparentThroatLocation = 1e-6;
+error.solver.M2relative = 1e-10;
+error.solver.M2absolute = 1e-10;
+error.dMdxDenominator = 4; % this is not an error tolerance, rather it is used to set the slope of dMdx in the transonic regime
+
+% ----------------------- SET NOZZLE GEOMETRY ----------------------------
+
+nozzle.geometry.shape = 'B-spline-mex';
+
+% knots and coefs defined at beginning of file
+
+% Determine nozzle throat
+[xThroat, yThroat] = BsplineGeometry(0, 'throat', nozzle.geometry.bSpline.knots, nozzle.geometry.bSpline.coefs);
+nozzle.geometry.xThroat = xThroat;
+
+% Define other geometry parameters
+nozzle.geometry.length = nozzle.geometry.bSpline.coefs(1,end);
+nozzle.inlet.D = nozzle.geometry.bSpline.coefs(2,1)*2;
+nozzle.geometry.Ainlet2Athroat = (nozzle.inlet.D/2)^2/yThroat^2;
+nozzle.geometry.Aexit2Athroat = (nozzle.geometry.bSpline.coefs(2,end))^2/yThroat^2;
+
+% -------------------- SET NOZZLE WALL GEOMETRY --------------------------
+nozzle.wall.shape = 'piecewise-linear';
+% nozzle.wall.seed = an array of the form [x; y] where [x,y]
+% denote the location of the control points with the origin being at
+% the center of the inlet area
+% nozzle.wall.breaks = vector giving location of breaks in piecewise
+% function
+nozzle.wall.seed = [0, 0.01; 
+                    nozzle.geometry.xThroat, 0.01; 
+                    nozzle.geometry.length, 0.01];
+nozzle.wall.breaks = [0;
+                      nozzle.geometry.xThroat;
+                      nozzle.geometry.length];
+                  
+% --------------------- FLUID INPUT PROPERTIES ---------------------------
+fluid.gam = 1.4; % ratio of specific heats
+fluid.R = 287.06; % J/kg-K, specific gas constant
+
+% ====================== RUN NOZZLE CALCULATIONS =========================
+
+% ----------------- CALCULATE FREESTREAM PROPERTIES ----------------------
+atm = StndAtm(altitude*0.3048,'SI'); % obtain standard atmosphere characteristics
+freestream.P = atm.P; % Pa, atmospheric pressure
+freestream.T = atm.T; % K, atmospheric temperature
+freestream.M = mach;
+freestream.U = freestream.M*sqrt(fluid.gam*fluid.R*freestream.T);
+
+[ nozzle ] = nozzleNonIdeal( fluid, freestream, nozzle, error );
+
+% ----------------- PRINT SOME USEFUL DATA TO SCREEN ---------------------
+fprintf('Nozzle Volume: %0.4f cm^3\n',nozzle.geometry.volume*100^3);
+fprintf('Nozzle Est. Thrust: %0.4f N\n',nozzle.approxThrust);
+fprintf('Nozzle Mass Flow Rate: %0.4f kg/s\n',nozzle.massFlowRate);
+
+% ============================ OUTPUT DATA ===============================
+
+if (strcmp(output,'nonlcon')) % nonlinear constraint functions for this case
+    Ceq = [];
+    C(1) = minRequiredThrust - nozzle.approxThrust; % should be <= 0
+    C(2) = minMassFlowRate - nozzle.massFlowRate; % should be <= 0
+    C(3) = nozzle.massFlowRate - maxMassFlowRate; % should be <= 0
+    
+    varargout = {C Ceq}; % required return format by fmincon
+
+elseif (strcmp(output,'volume')) % objective function for this case
+    if(isfloat(nozzle.geometry.volume) && nozzle.geometry.volume > 0)
+        varargout = {nozzle.geometry.volume};
+    else
+        fprintf('! Volume is not a positive float.\n')
+        varargout = {0};
+    end
+else
+    %varargout = {thrust, sfc, thermalEfficiency, engine};
+    error('other outputs currently not specified');
+end
+
+
