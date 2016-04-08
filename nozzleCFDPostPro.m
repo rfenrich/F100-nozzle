49b10d6b61fcea7e15c56b713dbaab631441cb1c
Rick Fenrich <rfenrich@stanford.edu>
2016-04-08 16:55:09 -0700

    updated what results are output by CFD code

--- nozzleCFDPostPro.m
+++ nozzleCFDPostPro.m
@@ -53,7 +53,7 @@ function [nozzle] = nozzleCFDPostPro(meshSU2, Sol, nozzle, fluid, freestream)
 	% ---    Interpolate solution to structured grid
 	% --------------------------------------------------------
 	
-	Ni=10;
+	Ni=200;
 	Nj=10;
 	
 	x = dat(:,ix);
@@ -108,28 +108,90 @@ function [nozzle] = nozzleCFDPostPro(meshSU2, Sol, nozzle, fluid, freestream)
 	
 	% --- Compute hoop stress
 	
+    % --- Extract pressure, temp, density, and velocity onto a grid
 	P = dat(:,iPres);
 	Pq = griddata(x,y,P,xq,yq);
-	
-	nozzle.hoopStress = prod([Pq(Nj,:) ;D(xq(1,:)')';1./(2*t(xq(1,:)')')]);
-
+    T = dat(:,iTem);
+    Tq = griddata(x,y,T,xq,yq);
+    rho = dat(:,iCons1);rho = dat(:,iCons1);
+    rhoq = griddata(x,y,rho,xq,yq);
+    U = dat(:,iCons2)./dat(:,iCons1);
+    Uq = griddata(x,y,U,xq,yq);
+    
 	% --- Compute
 	%WriteGMFStruc ('presin.mesh', xq, yq, Pq )
 	
 	% --------------------
 	% ---    Outputs      
 	% --------------------
-	
+    
+    % --- Calculate geometry
+    nozzle.xPosition = xq(1,:);
+    nozzle.geometry.A = A(nozzle.xPosition');
+    nozzle.geometry.dAdx = dAdx(nozzle.xPosition');
+    nozzle.geometry.D = D(nozzle.xPosition');
+    nozzle.wall.t = t(nozzle.xPosition);
+    nozzle.geometry.maxSlope = max(nozzle.geometry.dAdx./pi./nozzle.geometry.D);
+    nozzle.geometry.minSlope = min(nozzle.geometry.dAdx./pi./nozzle.geometry.D);
+    
+    % --- Assign flow data to nozzle
+    nozzle.flow.T = Tq;
+    nozzle.flow.P = Pq;
+    nozzle.flow.density = rhoq;
+    nozzle.flow.U = Uq;
+    nozzle.flow.M = Uq./sqrt(fluid.gam*fluid.R*Tq);
+    nozzle.flow.Tstag = nozzle.flow.T.*(1 + (fluid.gam-1).*nozzle.flow.M.^2/2); 
+    nozzle.flow.Pstag = nozzle.flow.P.*(1 + (fluid.gam-1).*nozzle.flow.M.^2/2).^(fluid.gam/(fluid.gam-1));
+    
+    % Sutherland's law for dynamic viscosity:
+    dynamicViscosity = @(T) 1.716e-5*(T/273.15).^1.5*(273.15 + 110.4)./(T + 110.4); % kg/m*s
+    nozzle.flow.Re = rhoq.*Uq.*repmat(nozzle.geometry.D,1,Nj)'./dynamicViscosity(nozzle.flow.T);
+    
+    % THE FOLLOWING 3 QUANTITIES NEED ESTIMATION
+    nozzle.flow.hf = 0;
+    nozzle.wallRecoveryFactor = 0;
+    nozzle.flow.Cf = 0;
+    
+    % --- Extract wall data
+    nozzle.Tw = nozzle.flow.T(Nj,:);
+    % THE FOLLOWING QUANTITY NEEDS ESTIMATION, EXTERNAL WALL TEMP
+    nozzle.Text = nozzle.Tw; % temporary fix
+
+    % --- Record nozzle exit values
 	nozzle.exit.M = avgDat(1,iMach);
 	nozzle.exit.T = avgDat(1,iTem);
 	nozzle.exit.U = (avgDat(1,iCons2)+avgDat(1,iCons3))/avgDat(1,iCons1);
 	nozzle.exit.P = avgDat(1,iPres);
 	nozzle.exit.Pstag = nozzle.exit.P*(1 + (fluid.gam-1)*nozzle.exit.M^2/2)^(fluid.gam/(fluid.gam-1));
-
-  nozzle.exit.Tstag = nozzle.exit.T*(1 + (fluid.gam-1)*nozzle.exit.M^2/2);
-  nozzle.netThrust = thrust;
-  nozzle.massFlowRate = mdot;
-  
+    nozzle.exit.Tstag = nozzle.exit.T*(1 + (fluid.gam-1)*nozzle.exit.M^2/2);    
+    
+    nozzle.PstagRatio = nozzle.exit.Pstag/nozzle.boundaryCdt.PtIn;
+    nozzle.TstagRatio = nozzle.exit.Tstag/nozzle.boundaryCdt.TtIn;
+    
+    % --- Assign thrust & mass flow rate values
+    nozzle.netThrust = thrust;
+    nozzle.massFlowRate = mdot;
+    
+    % --- Calculate stresses
+	%nozzle.hoopStress = prod([Pq(Nj,:) ;D(xq(1,:)')';1./(2*t(xq(1,:)')')]);
+    nozzle.hoopStress = Pq(Nj,:)'.*nozzle.geometry.D./(2*nozzle.wall.t);
+    nozzle.thermalHoopStress = 0.5*(nozzle.Tw-nozzle.Text)*nozzle.wall.coeffThermalExpansion*nozzle.wall.E/(1-nozzle.wall.poissonRatio);
+    nozzle.maxStress = nozzle.hoopStress' + nozzle.thermalHoopStress;    
+    
+    % --- Calculate cycles to failure Nf
+    nozzle.Nf = estimateNf(nozzle.Tw,nozzle.maxStress,1);
+	
+    % --- Calc nozzle material volume
+    % Volume calculation only works for spline parameterized nozzle geometry
+    % and piecewise-linear parameterized nozzle wall thickness
+    if(exist('pp','var')) % Exact volume for cubic spline parameterization
+        nozzle.geometry.volume = wallVolume(pp,nozzle.wall);
+    else % Approximate volume using trapezoidal integration
+        xVolume = linspace(0,nozzle.geometry.length,500)';
+        volumeIntegrand = pi*D(xVolume).*t(xVolume) + pi*t(xVolume).^2;
+        nozzle.geometry.volume = (xVolume(2)-xVolume(1))*trapz(volumeIntegrand);
+    end
+      
 	fprintf('\n -- Info: CFD results (averaged values at nozzle exit)\n');
 	fprintf('           Mach   = %f\n', nozzle.exit.M );
 	fprintf('           Temp   = %f K\n', nozzle.exit.T );
