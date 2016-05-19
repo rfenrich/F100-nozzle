faa8d1b9107ebf249dfae285aa1f5bcb95640fc1
Rick Fenrich <rfenrich@stanford.edu>
2016-05-18 17:18:35 -0700

    remove 10 iteration limit

--- writeSU2DataFile.m
+++ writeSU2DataFile.m
@@ -18,9 +18,7 @@ function [] = writeSU2DataFile ( nozzle )
 		nozzle.NbrIte = 600;
 	else 
 		nozzle.NbrIte = 300;
-	end
-	
-	nozzle.NbrIte = 10;
+    end
 	
   fprintf('	-- Writing SU2 datafile axinoz.cfg\n')
   fprintf('		Freestream Mach:               %f\n'   ,nozzle.boundaryCdt.Mref );
